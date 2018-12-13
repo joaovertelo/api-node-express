20c8ef6e17452074d8a3df5eae9eed6c7704963b
joaovertelo <joaovertelo@gmail.com>
2018-12-12 23:05:09 -0200

    ajustes na configuração do token

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv-safe").load();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -10,32 +11,36 @@ router.use(bodyParser.text());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(expressJWT({ secret: 'string de secret' }).unless({ path: [ '/api/login', '/api/register' ] }));
+
+app.use(expressJWT({ secret: process.env.SECRET }).unless({ path: ['/api/login', '/api/register'] }));
 
 const authAPI = require('./src/api/auth');
 const userAPI = require('./src/api/users');
 
+authAPI(router);
+
 router.use((req, res, next) => {
-	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+	var token = req.headers.authorization || req.headers['x-access-token'];
+	console.log(token)
 	if (token) {
-		jwt.verify(token, 'string de secret', (err, decoded) => {
+		jwt.verify(token.split(" ")[1], process.env.SECRET, (err, decoded) => {
 			if (err) {
-				return res.json({ success: false, message: 'Failed to authenticate token.' });
+				return res.status(500).send({ success: false, message: "Failed to authenticate token." });
 			} else {
 				req.decoded = decoded;
+				console.log(req.decoded)
 				next();
 			}
 		});
 	} else {
 		return res.status(403).send({
 			success: false,
-            message: 'No token provided.'
+			message: 'No token provided.'
 		});
 		//next();
 	}
 });
 
-authAPI(router);
 userAPI(router);
 
 app.use('/api', router);
--- src/api/auth.js
+++ src/api/auth.js
@@ -2,19 +2,25 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (router) => {
 	const login = (req, res) => {
-		const token = jwt.sign({ username: 'Joao' }, 'string de secret', { expiresIn: '3d' });
-		res.status(200).send({ auth: true, token });
+		const { user, pwd } = req.body;
+		if (user == "admin" && pwd === "admin") {
+			//auth success
+			const token = jwt.sign({ user: 'admin' }, process.env.SECRET, { expiresIn: '3d' });
+			res.status(200).send({ auth: true, token });
+		} else {
+			res.status(500).send("Login inválido")
+		}
 	};
 
-	router.get('/login', login);
+	router.post('/login', login);
 
 	router.get('/logout', (req, res) => {
 		res.status(200).send({ auth: false, token: null });
 	});
 
-	router.post('/register', function(req, res) {
+	router.post('/register', function (req, res) {
 		res.status(200).send({ auth: true, token: 'token' });
 	});
 
-	return router ;
+	return router;
 };
